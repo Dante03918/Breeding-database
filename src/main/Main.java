5edd15ba24e9e6c38ed6658ec5939511ccfd652c
dante03918 <pawel.albrecht01@gmail.com>
2019-11-05 19:50:48 +0100

    Root layout

---
+++ src/main/Main.java
@@ -0,0 +1,63 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+   private Stage primaryStage;
+   private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        this.primaryStage = primaryStage;
+        //this.primaryStage.setTitle("Informacje hodowlane");
+
+        initRootLayout();
+        showDetailedInformationLayout();
+    }
+
+    public void initRootLayout(){
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("rootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void showDetailedInformationLayout(){
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("layoutWithDetailedInformation.fxml"));
+            AnchorPane anchorPane = (AnchorPane) loader.load();
+
+            rootLayout.setCenter(anchorPane);
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
